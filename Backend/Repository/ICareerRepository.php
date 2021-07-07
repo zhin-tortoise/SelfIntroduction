5e11c842a3e3647adeae8e30ee8a3981c800b723
zhin-tortoise <34604212+zhin-tortoise@users.noreply.github.com>
2021-07-07 18:15:58 +0900

    経歴の更新機能の追加

--- Backend/Repository/ICareerRepository.php
+++ Backend/Repository/ICareerRepository.php
@@ -7,7 +7,9 @@
 interface ICareerRepository
 {
     public function createCareer(CareerEntity $careerEntity): string;
+    public function readCareerFromId(int $id);
     public function readCareerFromUserId(int $userId): array;
     public function readAllCareer(): array;
+    public function updateCareer(CareerEntity $careerEntity): string;
     public function deleteCareer(CareerEntity $careerEntity): string;
 }
